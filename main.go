9959a12c690f093c09371c71836e5ce52f528625
sina khalili <khalili@sfu.ca>
2021-06-29 12:37:29 -0700

    Add a bottom status bar

--- main.go
+++ main.go
@@ -378,6 +378,54 @@ func (m model) View() string {
 		fmt.Fprintf(&finale, "%s[%s] %s%s\n", pointer, checked, todoText, dirty)
 	}
 
+
+        // Status bar
+	{
+          var(
+                statusNugget = lipgloss.NewStyle().
+                                Foreground(lipgloss.Color("#FFFDF5")).
+                                Padding(0, 1)
+
+                statusBarStyle = lipgloss.NewStyle().
+                                Foreground(lipgloss.AdaptiveColor{Light: "#343433", Dark: "#C1C6B2"}).
+                                Background(lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#353533"})
+
+                statusStyle = lipgloss.NewStyle().
+                                Inherit(statusBarStyle).
+                                Foreground(lipgloss.Color("#FFFDF5")).
+                                Background(lipgloss.Color("#FF5F87")).
+                                Padding(0, 1).
+                                MarginRight(1)
+
+                encodingStyle = statusNugget.Copy().
+                                Background(lipgloss.Color("#A550DF")).
+                                Align(lipgloss.Right)
+
+                statusText = lipgloss.NewStyle().Inherit(statusBarStyle)
+
+                fishCakeStyle = statusNugget.Copy().Background(lipgloss.Color("#6124DF"))
+                width = 96
+              )
+
+		w := lipgloss.Width
+
+		statusKey := statusStyle.Render("STATUS")
+		encoding := encodingStyle.Render("UTF-8")
+		fishCake := fishCakeStyle.Render("🍥 Fish Cake")
+		statusVal := statusText.Copy().
+			Width(width - w(statusKey) - w(encoding) - w(fishCake)).
+			Render("Ravishing")
+
+		bar := lipgloss.JoinHorizontal(lipgloss.Top,
+			statusKey,
+			statusVal,
+			encoding,
+			fishCake,
+		)
+
+		finale.WriteString(statusBarStyle.Width(width).Render(bar))
+	}
+
 	return finale.String()
 }
 
